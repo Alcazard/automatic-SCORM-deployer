22940a753b27017b768e09623db0a6af1e9da9fc
theangi <matteo.angiari@gmail.com>
2018-08-04 13:37:20 +0200

    Refactoring ma non provato :)

--- Main.py
+++ Main.py
@@ -1,5 +1,3 @@
-# Work in progress
-
 import os
 import vimeo
 import json
@@ -7,129 +5,61 @@ from pathlib import Path
 from urllib.parse import urlparse
 import shutil
 
-# Directories for videos and SCORM template
-
-PATH_VIDEO_FOLDR = os.getcwd() # default to current directory
-
-SCORM_TEMPLATE = '/path/to/SCORM' # default
+ALLOWED_EXTENSION = ['.mp4', '.avi']
 
 
 def connect_to_vimeo():
-    print("Connessione a Vimeo in corso....")
+    print("Connecting to Vimeo....")
 
     config_file = os.path.dirname(os.path.realpath(__file__)) + '/config.json'
     config = json.load(open(config_file))
 
     if 'client_id' not in config or 'client_secret' not in config:
-        raise Exception('Non è stato possibile trovare il client_id o il client_secret ' +
-                        'nel file `' + config_file + '`. Controllare e riprovare.')
-    else:
-        print(config)
+        raise ConnectionError('No client_id or client_secret found in {}'.format(config_file))
 
-    # Instantiate the library with your client id, secret and access token
-    v = vimeo.VimeoClient(
+    vimeo_client = vimeo.VimeoClient(
         token=config['access_token'],
         key=config['client_id'],
         secret=config['client_secret']
     )
 
-    # Make the request to the server for the "/me" endpoint.
-    about_me = v.get('/me')
+    about_me = vimeo_client.get('/me')  # Make the request to the server for the "/me" endpoint.
 
-    # Make sure we got back a successful response.
-    assert (about_me.status_code == 200)
+    if about_me.status_code != 200:
+        raise ValueError('Cannot get /me, status is {}'.format(about_me.status_code))
 
-    # Load the body's JSON data.
-    #print (about_me.json())
-
-    return v
+    return vimeo_client
 
 
-def upload_popup_mode(key_auth, path_video):
-
-    # Extracting video name for upload
-
+def upload_video(key_auth, path_video, video_title_suffix=''):
     video_name = os.path.basename(path_video)
+    video_title = os.path.splitext(video_name)[0] + video_title_suffix
+    print('Uploading video "{}", title is {}'.format(video_name, video_title))
 
-    print ('Uploading: %s' % (video_name))
-
-    # Extracting title
-
-    video_title = os.path.splitext(video_name)[0]
-
+    # Change the preset of the video (take it as a parameter!
+    # key_auth.patch(uri, data={'name': 'Video title', 'description': '...'})
 
     try:
-        # Upload the file and include the video title and description.
         uri = key_auth.upload(path_video, data={
-            'name': '%s' % video_title,
+            'name': str(video_title),
             'description': "Questo video è stato caricato con Automatic SCORM Deployer"
         })
 
-        # Get the metadata response from the upload and log out the Vimeo.com url
-        video_data = key_auth.get(uri + '?fields=link').json()
-        print ('"%s" has been uploaded to %s' % (video_name, video_data['link']))
+        video_metadata = key_auth.get(uri + '?fields=link').json()
+        print('"{}" has been uploaded to {}'.format(video_name, video_metadata['link']))
 
-
-
-    except vimeo.exceptions.VideoUploadFailure as e:
-        # We may have had an error. We can't resolve it here necessarily, so
-        # report it to the user.
-        print ('Error uploading %s' % (video_name))
-        print ('Server reported: %s' % (e.message))
-
-    print(video_data)
-    print(video_data['link'])
-    # return the parsed url
-
-    parsed = urlparse(video_data['link'])
-
-    return parsed.path.replace("/", "")
-
-
-def upload_completed_mode(key_auth, path_video):
-
-    # Extracting video name for upload
-
-    video_name = os.path.basename(path_video)
-
-    print('Uploading: %s' % (video_name))
-
-    # Extracting title and adding ending
-
-    video_title = os.path.splitext(video_name)[0] + "_completato"
-
-    try:
-        # Upload the file and include the video title and description.
-        uri = key_auth.upload(path_video, data={
-            'name': '%s' % video_title,
-            'description': "Questo video è stato caricato con Automatic SCORM Deployer"
-        })
-
-        # Get the metadata response from the upload and log out the Vimeo.com url
-        video_data = key_auth.get(uri + '?fields=link').json()
-        print('"%s" has been uploaded to %s' % (video_name, video_data['link']))
-
-        # Change the preset of the video
-        #key_auth.patch(uri, data={'name': 'Video title', 'description': '...'})
+        return video_metadata
 
     except vimeo.exceptions.VideoUploadFailure as e:
-        # We may have had an error. We can't resolve it here necessarily, so
-        # report it to the user.
-        print('Error uploading %s' % (video_name))
-        print('Server reported: %s' % (e.message))
+        print('Error uploading {}, server said: {}'.format(video_name, e.message))
 
-    # return the parsed url
 
-    parsed = urlparse(video_data['link'])
-
-    return parsed.path.replace("/", "")
-
+def get_vimeo_id(metadata):
+    return urlparse(metadata['link']).path.replace("/", "")
 
 
 def generate_scorm(popup_id, completed_id, out_foldr):
-
-    # Search for config.json
-    json_file = []
+    json_file = []      #FIXME: Umh...
 
     for folder, subs, files in os.walk(out_foldr):
         for filename in files:
@@ -137,75 +67,59 @@ def generate_scorm(popup_id, completed_id, out_foldr):
                 json_file.append(os.path.abspath(os.path.join(folder, filename)))
 
     config_file = ''.join(json_file)
-    with open(config_file, 'r') as JSON:
-        data = json.load(JSON)
+
+    with open(config_file, 'r') as scorm_json_file:
+        data = json.load(scorm_json_file)
 
         if 'VIDEO_ID' not in data or 'VIDEO_ID_COMPLETED' not in data:
-            raise Exception('Il Template SCORM nno è valido. Controllare e riprovare.')
-        else:
-            print(data)
+            raise Exception('Invalid Scorm Template!. Aborting')
 
-        data["VIDEO_ID"]= popup_id
+        data["VIDEO_ID"] = popup_id
         data["VIDEO_ID_COMPLETED"] = completed_id
-        print(data)
 
-    with open(config_file, 'w') as JSON:
-        JSON.write(json.dumps(data))
+        print('Generating SCORM with following data: {}'.format(data))
 
-    # Archive the SCORM # Todo
+    # FIXME: I think you could open and write the file at the same time!
+    with open(config_file, 'w') as scorm_json_file:
+        scorm_json_file.write(json.dumps(data))
 
     shutil.make_archive(out_foldr, 'zip', os.path.dirname(os.path.realpath(out_foldr)))
 
-    return(0)
 
-def main():
-    out_foldr = PATH_VIDEO_FOLDR # default
-
+def main(path_video_folder=os.getcwd(), path_scorm_template='/path/to/SCORM'):
+    out_folder = path_video_folder
     key_auth = connect_to_vimeo()
+    files_path = []
 
-    file_paths = []
-
-    for folder, subs, files in os.walk(PATH_VIDEO_FOLDR):
+    # FIXME: This should be a list comprehension :) # files = [x for x in os.walk(PATH_VIDEO_FOLDR) if x.endswith( ...]
+    for folder, subs, files in os.walk(path_video_folder):
         for filename in files:
-            file_paths.append(os.path.abspath(os.path.join(folder, filename)))
+            file_extension = os.path.splitext(filename)[1]
+            if file_extension in ALLOWED_EXTENSION:
+                files_path.append(os.path.abspath(os.path.join(folder, filename)))
 
-    for filename in file_paths:
-        if filename.endswith(".mp4") or filename.endswith(".avi"):
-            print('Processo file {}'.format(filename))
+    # FIXME: Merge this for with the previous: Do a single for loop! :)
+    for filename in files_path:
+        print('Processing file {}'.format(filename))
+        abs_video_path = Path(filename).resolve()
 
-            # We need the absolute path for the video
+        popup_video_metadata = upload_video(key_auth, str(abs_video_path))
+        popup_id = get_vimeo_id(popup_video_metadata)
 
-            video_path = Path(filename).resolve()
-            print(video_path)
+        completed_video_metadata = upload_video(key_auth, str(abs_video_path), '_completato')
+        completed_id = get_vimeo_id(completed_video_metadata)
 
-            popup_id = upload_popup_mode(key_auth, str(video_path))
-            completed_id = upload_completed_mode(key_auth, str(video_path))
+        print('Upload process completed: popup id is {} and completed id is {}'.format(popup_id, completed_id))
+        try:
+            file_name = os.path.basename(filename)
+            file_title = os.path.splitext(file_name)[0]
 
-            try:
-                # Extracting file name
+            shutil.copytree(path_scorm_template, file_title)
+            generate_scorm(popup_id, completed_id, shutil.move(file_title, out_folder))
 
-                file_name = os.path.basename(filename)
-                print(file_name)
-                # Extracting file title
-
-                file_title = os.path.splitext(file_name)[0]
-                print(file_title)
-                # We copy the SCORM template in the video directory
-
-                shutil.copytree(SCORM_TEMPLATE, file_title)
-
-                generate_scorm(popup_id,completed_id,shutil.move(file_title, out_foldr))
-
-            except FileExistsError:
-                print("Il File esiste già!")
-
-
-
-        else :
-             print("Trovato file non processabile {}" .format(filename))
-
-
+        except FileExistsError:
+            print("File {} Already Exists!".format(filename))
 
 if __name__ == '__main__':
-
     main()
+    print('Process completed.')

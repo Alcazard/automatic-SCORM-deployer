1aef36e5019dee7581aa23891c0ec10addcca2a1
theangi <matteo.angiari@gmail.com>
2018-08-09 17:58:14 +0200

    Little refactors, <= 100 LoC

--- Main.py
+++ Main.py
@@ -4,14 +4,10 @@ import json
 from urllib.parse import urlparse
 import shutil
 
-
 ALLOWED_EXTENSION = ('.mp4', '.avi')
 
 
-def connect_to_vimeo():
-    print("Connecting to Vimeo....")
-
-    config_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'config.json')
+def connect_to_vimeo(config_file):
     config = json.load(open(config_file))
 
     if 'client_id' not in config or 'client_secret' not in config:
@@ -34,7 +30,6 @@ def connect_to_vimeo():
 def upload_video(key_auth, path_video, video_title_suffix=''):
     video_name = os.path.basename(path_video)
     video_title = os.path.splitext(video_name)[0] + video_title_suffix
-    print('Uploading video "{}", title is {}'.format(video_name, video_title))
 
     try:
         uri = key_auth.upload(path_video, data={
@@ -46,9 +41,8 @@ def upload_video(key_auth, path_video, video_title_suffix=''):
         print('"{}" has been uploaded to {}'.format(video_name, video_metadata['link']))
 
         if video_title_suffix is '_completato':
-            print('Change preset to completed...maybe')
+            print('Changing preset for video...')
             key_auth.put(uri + '/presets/120447750')
-
         return video_metadata
 
     except vimeo.exceptions.VideoUploadFailure as e:
@@ -59,29 +53,30 @@ def get_vimeo_id(metadata):
     return urlparse(metadata['link']).path.replace("/", "")
 
 
-def generate_scorm(popup_id, completed_id, out_foldr):
+def generate_scorm(popup_id, completed_id, out_foldr, compress_type='zip'):
     config_file = os.path.join(out_foldr, 'config.json')
 
     with open(config_file, 'r') as scorm_json_file:
         data = json.load(scorm_json_file)
 
         if 'VIDEO_ID' not in data or 'VIDEO_ID_COMPLETED' not in data:
-            raise Exception('Invalid Scorm Template!. Aborting')
+            raise ValueError('Invalid Scorm Template!. Aborting')
 
         data["VIDEO_ID"] = popup_id
         data["VIDEO_ID_COMPLETED"] = completed_id
 
-        print('Generating SCORM with following data: {}'.format(data))
+    with open(config_file, 'w') as scorm_json_file:
+        scorm_json_file.write(json.dumps(data))
 
-    with open(config_file, 'w') as JSON:
-        JSON.write(json.dumps(data))
-
-    shutil.make_archive(out_foldr, 'zip', out_foldr)
+    shutil.make_archive(out_foldr, compress_type, out_foldr)
 
 
-def main(path_video_folder='/path/to/video/directory', path_scorm_template='/path/to/SCORM/template'):
-    out_folder = path_video_folder
-    key_auth = connect_to_vimeo()
+def main(path_video_folder='/path/to/video/directory',
+         path_scorm_template='/path/to/SCORM/template',
+         vimeo_config_file=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'config.json')):
+
+    print("Connecting to Vimeo....")
+    key_auth = connect_to_vimeo(vimeo_config_file)
 
     for filename in [f for f in os.listdir(path_video_folder) if f.endswith(ALLOWED_EXTENSION)]:
         print('Processing file {}'.format(filename))
@@ -93,17 +88,16 @@ def main(path_video_folder='/path/to/video/directory', path_scorm_template='/pat
         completed_video_metadata = upload_video(key_auth, str(abs_video_path), '_completato')
         completed_id = get_vimeo_id(completed_video_metadata)
 
-        print('Upload process completed: popup id is {} and completed id is {}'.format(popup_id, completed_id))
+        print('Upload for {} completed: popup id: {}, completed id: {}'.format(filename, popup_id, completed_id))
         try:
             file_name = os.path.basename(filename)
             file_title = os.path.splitext(file_name)[0]
 
             shutil.copytree(path_scorm_template, file_title)
-            generate_scorm(popup_id, completed_id, shutil.move(file_title, out_folder))
+            generate_scorm(popup_id, completed_id, shutil.move(file_title, path_video_folder))
 
         except FileExistsError:
             print("File {} Already Exists!".format(filename))
 
 if __name__ == '__main__':
     main()
-    print('Process completed.')
